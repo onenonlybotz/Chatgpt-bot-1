4770be95969568ebe07ed4615c6a062af1262f49
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-03-22 15:14:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 API_ID = os.environ.get("API_ID", "16051908") 
 API_HASH = os.environ.get("API_HASH", "abf9b83f9ca40cf9f5ba9bf6e6afaa8b") 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5956160709:AAHPTf3PfjgSrJ8FDlpm_qkp7jhHg927y7A") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
 OPENAI_KEY = os.environ.get("OPENAI_KEY", None)
 BOT_USERNAME = os.environ.get("BOT_USERNAME" ,"Sexyaadityabot") 
 SUDO=os.environ.get("SUDO","5307865914")
-BOT_NAME = os.environ.get("BOT_NAME","AADITYA PVT CHATGPT")
+BOT_NAME = os.environ.get("BOT_NAME","AADITYA AI")
 START_IMG = os.environ.get("START_IMG","https://graph.org/file/c66656541bb0d93679a91.jpg")
 STKR = os.environ.get("STKR","CAACAgUAAx0Cbc0hiAACCCdkGV8SLh6npK6iqSIjFAAB-YeJG-gAAlcIAAIHn-FU-nC7zlMxNzQvBA")
 BANNED_USERS=os.environ.get("BANNED_USERS",None)
